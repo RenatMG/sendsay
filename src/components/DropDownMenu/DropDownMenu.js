85d17d9b61920872494ca6613577348e829b7eed
Renat Honor <renamga@gmail.com>
2020-08-21 10:40:13 +0400

    little change in drop menu

--- src/components/DropDownMenu/DropDownMenu.js
+++ src/components/DropDownMenu/DropDownMenu.js
@@ -7,7 +7,7 @@ const DropDownMenu = ({top = 0, left = 0, width = 0, scroll}) => {
     return (
         <div className={classes.menu} style={{top: top + 30, left, width, display: scroll ? 'none' : 'block'}}>
             <div className='d-flex flex-column'>
-                <div className={classes.item}>Выполнить</div>
+                <div className={classes.item + ' ' + classes.primary}>Выполнить</div>
                 <div className={classes.item}>Скопировать</div>
                 <Divider/>
                 <div className={classes.item}>Удалить</div>
